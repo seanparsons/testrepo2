0c1f2779f166bc55a9b63c4d488494bd835b9bf7
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2023-09-07 14:38:26 +0100

    Update storyboard.js

--- utopia/storyboard.js
+++ utopia/storyboard.js
@@ -25,7 +25,7 @@ export var storyboard = (
         left: 1036,
         top: 128,
       }}
-      data-label='New Label'
+      data-label='New And Improved Label'
     >
       <App style={{}} />
     </Scene>
